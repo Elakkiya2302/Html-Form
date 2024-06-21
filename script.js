ce79227c561fd700cccc19dc972b0a307da175b3
Elakkiya2302 <148260220+Elakkiya2302@users.noreply.github.com>
2024-06-21 14:52:01 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+function foo(){
+    var name = document.getElementById("Firstname").value;
+    var name1 = document.getElementById("Lastname").value;
+    var name2 = document.getElementById("Address Line1").value;
+    var name3 = document.getElementById("Address Line2").value;
+    var name4 = document.getElementById("Pin").value;
+     var name5 = document.getElementById("Gender").value;
+     var name6 = document.getElementById("Choice of Food").value;
+    var name7 = document.getElementById("State").value;
+    var name8 = document.getElementById("Country").value;
+    console.log("Firstname:"+name);
+    console.log("Lastname:"+name1);
+    console.log("Address Line1:"+name2);
+    console.log("Address Line2:"+name3);
+    console.log("Pin:"+name4);
+     console.log("Gender:"+name5);
+     console.log("Choice of Food:"+name6);
+    console.log("State:"+name7);
+    console.log("Country:"+name8);
+}
+
